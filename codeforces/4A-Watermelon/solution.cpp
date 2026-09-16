e7d0cf555923104f7cb74db85c74f21d5053a9d6
João Cruz <joao.emanuel.inv@gmail.com>
2026-09-15 23:34:56 -0300

    Solve CF 4A - Watermelon

---
+++ codeforces/4A-Watermelon/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int w {};
+    std::cin >> w;
+
+    if (w <= 2)
+    {
+        std::cout << "NO\n";
+    }
+    else if (w % 2 == 0)
+        std::cout << "YES\n";
+    else std::cout << "NO\n";
+    return 0;
+}
